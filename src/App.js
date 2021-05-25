2a429c463b0b0b90542f1be041497dedcc56fc96
Aftaab Dobani <1234dobani@gmail.com>
2021-05-24 22:52:51 -0400

    called bookcards in app

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import './App.css';
 import Header from './components/Header/header';
 import Books from './components/Books/Books';
+import Bookcard from './components/Bookcard/Bookcard';
 
 class App extends Component {
   render() {
@@ -9,6 +10,7 @@ class App extends Component {
       <div className="App">
         <Header />
         <Books />
+        <Bookcard />
       </div>
     )
   }
