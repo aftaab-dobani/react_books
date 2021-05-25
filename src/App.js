f408a9b99ee74805251957f7be992a6018f9ebf0
Aftaab Dobani <1234dobani@gmail.com>
2021-05-24 23:22:38 -0400

    added styling

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 import './App.css';
 import Header from './components/Header/header';
 import Books from './components/Books/Books';
-import Bookcard from './components/Bookcard/Bookcard';
+// import Bookcard from './components/Bookcard/Bookcard';
 
 class App extends Component {
   render() {
@@ -10,7 +10,7 @@ class App extends Component {
       <div className="App">
         <Header />
         <Books />
-        <Bookcard />
+        {/* <Bookcard /> */}
       </div>
     )
   }
