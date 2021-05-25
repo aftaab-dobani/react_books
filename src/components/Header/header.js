0541fe130fa7276f6719360dfa2d658be4c827f4
Aftaab Dobani <1234dobani@gmail.com>
2021-05-24 20:05:09 -0400

    added header component

---
+++ src/components/Header/header.js
@@ -0,0 +1,12 @@
+import React from 'react'; 
+import "./style.css"
+
+const Header = () => {
+    return(
+        <header>
+            <h1>React Books</h1>
+        </header>
+    )
+}
+
+export default Header; 
---
+++ src/components/Search/Search.js
@@ -0,0 +1,12 @@
+import React from 'react'; 
+import "./style.css"
+
+const Search = () => {
+    return(
+        <div className="search-area">
+            
+        </div>
+    )
+}
+
+export default Search; 
